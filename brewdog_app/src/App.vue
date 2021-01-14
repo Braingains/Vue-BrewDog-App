4e60e3a80bc6868b02fa2c584d2ed918d067a42a
Ewan Brain <ewan.brain@gmail.com>
2021-01-14 16:28:20 +0000

    all beers now showing from all API pages

--- brewdog_app/src/App.vue
+++ brewdog_app/src/App.vue
@@ -19,6 +19,9 @@ export default {
       beers: [],
       beers1: [],
       beers2: [],
+      beers3: [],
+      beers4: [],
+      beers5: [],
       selectedBeer : null,
       favourites :[],
       beerToSave : null
@@ -29,12 +32,10 @@ export default {
 // Promise.all([promise1, promise2, promise3]).then((values) => {
   // console.log(values);
 // });
-    fetch('https://api.punkapi.com/v2/beers?page=2&per_page=80')
-    .then(res => res.json())
-    .then(beers => this.beers = beers);
-    // Promise.all([fetch('https://api.punkapi.com/v2/beers?page=1&per_page=80'), fetch('https://api.punkapi.com/v2/beers?page=2&per_page=80'),
-    // fetch('https://api.punkapi.com/v2/beers?page=3&per_page=80'),
-    // fetch('https://api.punkapi.com/v2/beers?page=4&per_page=80')]) 
+    
+
+
+    // fetch('https://api.punkapi.com/v2/beers?page=1&per_page=80')
     // .then(res => res.json())
     // .then(beers => this.beers1 = beers);
 
@@ -44,19 +45,19 @@ export default {
 
     // fetch('https://api.punkapi.com/v2/beers?page=3&per_page=80')
     // .then(res => res.json())
-    // .then(beers => this.beers.push(beers));
+    // .then(beers => this.beers3 = beers);
 
     // fetch('https://api.punkapi.com/v2/beers?page=4&per_page=80')
     // .then(res => res.json())
-    // .then(beers => this.beers.push(beers));
+    // .then(beers => this.beers4 = beers);
 
     // fetch('https://api.punkapi.com/v2/beers?page=5&per_page=80')
     // .then(res => res.json())
-    // .then(beers => this.beers.push(beers));
+    // .then(beers => this.beers5 = beers);
 
-    // function (beers1, beers2){
-    //   beers.shift(beers1, beers2)
-    // }
+    
+    this.getAllBeers();
+    
 
     eventBus.$on('beer-selected', (beer) => {
       this.selectedBeer = beer
@@ -71,8 +72,30 @@ export default {
       const index = this.favourites.indexOf(beer)
       this.favourites.splice(index, 1)
     })
+    //beer from favourites is taken from eventsBus,
+    //we get indexOf that beer,
+    //we give that index to .splice and remove (1) item at that index
+
   },
   methods: {
+
+    getAllBeers: function(){
+      const promises = [1,2,3,4,5].map(number =>{
+        return fetch(`https://api.punkapi.com/v2/beers?page=${number}&per_page=80`)
+        .then(res => res.json())
+      })
+      console.log(promises);
+      Promise.all(promises)
+        .then(data => {
+          const beerInfo = data.reduce((beers, beersToAdd) => {
+            return beers.concat(beersToAdd)
+          }, [])
+
+          this.beers = beerInfo
+        })
+        
+
+    }
     
 },
   components: {
@@ -89,6 +112,12 @@ export default {
   .main-container {
     display: flex;
     justify-content: space-between;
+    background-image: url("https://media3.giphy.com/media/V6vYGxjArFFde/giphy.gif?cid=ecf05e4712qiv49f8u8s00362p60h74i5rwtfcr6l8hh7cdt&rid=giphy.gif");
+    background-color: #5229037b;
+    background-repeat:no-repeat;
+    background-size:contain;
+    color: lemonchiffon; 
+    
   }
-
+ 
 </style>
