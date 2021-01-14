d8b13f75bacaa40ade34a254151816ccbe7eff04
Ewan Brain <ewan.brain@gmail.com>
2021-01-14 10:57:32 +0000

    api imported|

---
+++ brewdog_app/src/App.vue
@@ -0,0 +1,25 @@
+<template lang='html'>
+  <p>helllloooo</p>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data (){
+    return {
+      beers: [],
+    }
+
+  },
+  mounted(){
+    fetch('https://api.punkapi.com/v2/beers')
+    .then(res => res.json())
+    .then(beers => this.beers = beers);
+  }
+
+}
+</script>
+
+<style>
+
+</style>
